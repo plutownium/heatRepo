f5f7e18f20f3de7d68d902096e1e0f8155175b02
Roland Mackintosh <rolandmackintosh@outlook.com>
2022-01-27 20:00:35 -0800

    rewriting models for the 4th time

--- mysite/heat/db/db.py
+++ mysite/heat/db/db.py
@@ -1,10 +1,14 @@
+from sqlalchemy import Table, Column, Integer, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import event
+
 import sqlalchemy as db
+
 # engine = db.create_engine('dialect+driver://user:pass@host:port/db')
 
 # fixme: dialect + driver user:pass
 
 engine = db.create_engine('sqlite:///census.sqlite')
 connection = engine.connect()
-# metadata = db.MetaData()
-# census = db.Table('census', metadata, autoload=True, autoload_with=engine)
 
+Base = declarative_base()
--- mysite/heat/models.py
+++ mysite/heat/models.py
@@ -1,3 +1,64 @@
 from django.db import models
+from django.db.models import CharField, IntegerField, PositiveIntegerField, ForeignKey
 
 # Create your models here.
+
+class Viewer(models.Model):
+    __tablename__ = 'viewer'
+    id = PositiveIntegerField(primary_key=True)
+
+
+class Session(models.Model):
+    __tablename__ = "session"
+    session_id = PositiveIntegerField(primary_key=True),
+    user_id = ForeignKey("user_account.user_id"),
+    ip = CharField(max_length=30),
+    height = IntegerField()
+    width = IntegerField()
+
+
+class XYCoord(models.Model):
+    __tablename__ = "xyCoords"
+    session_id = ForeignKey("session.session_id")
+    user_id = PositiveIntegerField(primary_key=True)
+    ip = CharField(max_length=30)
+    x = IntegerField()
+    y = IntegerField()
+
+
+class ScrollEvent(models.Model):
+    __tablename__ = "scroll_event"
+    session_id = ForeignKey("session.session_id")
+    user_id = PositiveIntegerField(primary_key=True)
+    ip = CharField(max_length=30)
+    location = IntegerField()
+
+
+class CaptureTouch(models.Model):
+    __tablename__ = "capture_touch"
+    session_id = ForeignKey("session.session_id")
+    user_id = PositiveIntegerField(primary_key=True)
+    location = IntegerField()
+
+
+class LogScreenDimensions(models.Model):
+    __tablename__ = "screen_dimensions_log"
+    session_id = ForeignKey("session.session_id")
+    user_id = PositiveIntegerField(primary_key=True)
+    height = IntegerField()
+    width = IntegerField()
+
+
+class InactiveSession(models.Model):
+    __tablename__ = "inactive_session"
+    session_id = ForeignKey("session.session_id")
+    user_id = PositiveIntegerField(primary_key=True)
+    height = IntegerField()
+    width = IntegerField()
+
+
+class ConversionEvent(models.Model):
+    __tablename__ = "conversion_event"
+    session_id = ForeignKey("session.session_id")
+    conversion_id = IntegerField()
+
